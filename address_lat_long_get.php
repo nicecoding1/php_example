272580e274bf9d99aacf529d62a79f0e594b0987
Jaesang Jo <nicecoding@naver.com>
2024-10-01 12:02:25 +0900

    Update address_lat_long_get.php

--- address_lat_long_get.php
+++ address_lat_long_get.php
@@ -1,13 +1,13 @@
 <?php
 
 function get_lat_long($address) {
-	$apiUrl = "https://address.dawul.co.kr/input_pro.php";
-	$curl = curl_init();
+    $apiUrl = "https://address.dawul.co.kr/input_pro.php";
+    $curl = curl_init();
 
     curl_setopt_array($curl, array(
         CURLOPT_URL => $apiUrl,
-		CURLOPT_SSL_VERIFYPEER => 0,
-		CURLOPT_SSL_VERIFYHOST => 0,
+	CURLOPT_SSL_VERIFYPEER => 0,
+	CURLOPT_SSL_VERIFYHOST => 0,
         CURLOPT_RETURNTRANSFER => 1,
         CURLOPT_POST => 1,
         CURLOPT_POSTFIELDS => 'refine_ty=8&protocol_='.urlencode($address),
@@ -19,18 +19,18 @@ function get_lat_long($address) {
 
     $response = curl_exec($curl);
     curl_close($curl);
-	$temp = explode("|", $response);
-	$arr[0] = $temp[4];
-	$arr[1] = $temp[3];
+    $temp = explode("|", $response);
+    $arr[0] = $temp[4];
+    $arr[1] = $temp[3];
 
     //서울특별시 중구 한강대로 405  (봉래동2가, 경부고속철도서울민자역사)|서울특별시 중구 봉래동2가 122-11 |04509|126.969422|37.5562730
-	/*
-	서울특별시 중구 한강대로 405  (봉래동2가, 경부고속철도서울민자역사)
-	서울특별시 중구 봉래동2가 122-11
-	04509
-	126.969422
-	37.5562730
-	*/
+    /*
+    서울특별시 중구 한강대로 405  (봉래동2가, 경부고속철도서울민자역사)
+    서울특별시 중구 봉래동2가 122-11
+    04509
+    126.969422
+    37.5562730
+    */
 
     return $arr;
 }
